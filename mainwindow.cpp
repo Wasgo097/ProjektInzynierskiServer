d277d414e8d19f7dd10315e0151013c9903a4b10
Wasgo097 <patrykpakula321@gmail.com>
2021-03-29 10:46:31 +0200

    Basic udp communication via udp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,53 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
+MainWindow::MainWindow(QWidget *parent): QMainWindow(parent), ui(new Ui::MainWindow){
     ui->setupUi(this);
+    connect(&Timer,&QTimer::timeout,this,&MainWindow::timeout);
+    connect(&Socket,&QUdpSocket::readyRead,this,&MainWindow::readyread);
+    Timer.setInterval(1000);
+    start();
 }
-
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow(){
     delete ui;
 }
 
+void MainWindow::start()
+{
+    if(!Socket.bind(port)){
+        qInfo()<<Socket.errorString();
+        return;
+    }
+    qInfo()<<"Started udp on "<<Socket.localAddress()<<" : "<<Socket.localPort();
+    broadcast();
+}
+
+void MainWindow::stop()
+{
+    Timer.stop();
+    Socket.close();
+    qInfo()<<"stopped";
+}
+
+void MainWindow::timeout()
+{
+    QString date=QDateTime::currentDateTime().toString();
+    QByteArray data=date.toLatin1();
+    QNetworkDatagram datagram(data,QHostAddress::Broadcast,port);
+    qInfo()<<"Send: "<<data<<" my address "<<datagram.senderAddress();
+    Socket.writeDatagram(datagram);
+}
+
+void MainWindow::readyread()
+{
+    while(Socket.hasPendingDatagrams()){
+        QNetworkDatagram datagram=Socket.receiveDatagram();
+        qInfo()<<"Read: "<<datagram.data()<<" from "<<datagram.senderAddress()<< " port "<<datagram.senderPort();
+    }
+}
+
+void MainWindow::broadcast()
+{
+    qInfo()<<"Broadcasting";
+    Timer.start();
+}
--- mainwindow.h
+++ mainwindow.h
@@ -1,21 +1,32 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-
 #include <QMainWindow>
-
+#include <QDebug>
+#include <QTimer>
+#include <QDateTime>
+#include <QUdpSocket>
+#include <QNetworkDatagram>
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
-
-class MainWindow : public QMainWindow
-{
+class MainWindow : public QMainWindow{
     Q_OBJECT
 
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
-
 private:
     Ui::MainWindow *ui;
+public slots:
+    void start();
+    void stop();
+    void timeout();
+    void readyread();
+    void broadcast();
+private:
+    QUdpSocket Socket;
+    QTimer Timer;
+    quint16 port=2020;
+
 };
 #endif // MAINWINDOW_H
